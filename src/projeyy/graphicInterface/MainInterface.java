cc7eaff175fe3c231a0fa9b0d855febe4b65f13c
Loïc Wisniewski <metaheavens@gmail.com>
2015-10-18 15:23:11 +0200

    Changement des nom des packages et des classes de l'interface

---
+++ src/projeyy/graphicInterface/MainInterface.java
@@ -0,0 +1,12 @@
+package projeyy.graphicInterface;
+import javax.swing.JFrame;
+
+
+
+@SuppressWarnings("serial")
+public class MainInterface extends JFrame {
+
+	public static void main(String[] args){
+		JFrame fenetre = new FenetreMenu();
+	}
+}
