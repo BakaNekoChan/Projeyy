b230e7b738e618e9dafa662c673899ba3274d0a4
Lucie <lucie.borrossi@hotmail.fr>
2015-12-08 09:44:18 +0100

    new ArbreTrie

--- src/projeyy/mst2/ArbreTrie.java
+++ src/projeyy/mst2/ArbreTrie.java
@@ -1,7 +1,9 @@
 package projeyy.mst2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 
@@ -9,58 +11,42 @@ public class ArbreTrie {
 
 	
 	private TreeSet<CoupleDis> ts;
+	private final int nbVille;
 	
 	//rempli ts en triant les arêtes par distance
 	public ArbreTrie(double [][] m){
-		for (int i=0; i<m.length; i++){
+		nbVille=m.length;
+		for (int i=0; i<nbVille; i++){
 			for(int j=0; j<i; j++){
 				ts.add(new CoupleDis(i, j, m[i][j]));
 			}
 		}
 	}
 	
-	
-	
-	
 	//retourne une liste d'arêtes correspondant à un arbre couvrant minimum
-	public static List<CoupleDis> minCircuit(TreeSet<CoupleDis> ts){
-		ArrayList<CoupleDis> aretes = new ArrayList<CoupleDis>();
-		ArrayList<Integer> sousTree = new ArrayList<Integer>();
-		aretes.add(ts.first());
-		sousTree.add(ts.first().getI());
-		sousTree.add(ts.first().getJ());		
+	public List<CoupleDis> minArbreCouvrant(){
+		ArrayList<CoupleDis> list = new ArrayList<CoupleDis>();
+		HashSet<Integer>[] connexClasse = new HashSet<Integer>[]() ;
+		for (int i=0; i<nbVille; i++) {
+			connexClasse[i] = new HashSet<Integer>();
+			connexClasse[i].add(i);
+		}
+		//ajout arete (i,j)
+		connexClasse[ts.first().getI()].addAll(connexClasse[ts.first().getJ()]);
+		connexClasse [ts.first().getJ()].add(ts.first().getI());
+		list.add(ts.first());
 		for (CoupleDis cur : ts){
-			if ( (sousTree.contains(cur.getI()) && !sousTree.contains(cur.getJ())) ){
-				aretes.add(cur);
-				sousTree.add(cur.getJ());
-				cur=ts.first();
+			if(!connexClasse[cur.getJ()].contains(cur.getI())){
+				connexClasse[cur.getI()].addAll(connexClasse[cur.getJ()]);
+				for (int j2 : connexClasse[cur.getJ()]){
+					connexClasse[j2]=connexClasse[cur.getI()];
+				}
+				list.add(cur);
 			}
-			if ( (!sousTree.contains(cur.getI()) && sousTree.contains(cur.getJ())) ) {
-				aretes.add(cur);
-				sousTree.add(cur.getI());
-				cur=ts.first();
-			}
-		}		
-		return aretes;
+		}
+		return list;
 	}
 	
 }
 
-/*set<Integer>[] connexClass = new set<Integer>[n]
- * for (i) {
- * connexClasse[i] = new HashSet;
- * connexClasse.add(i);
- */
-// ajout arÃªte (i,j)
-/*
- * connexClasse[i].addAll(connexClasse[j])
- *for (int j2 : connexClasse[j])
- *connexClasse[j2]=connexClasse[i];
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
+
