e817ac748461ad710376dbb6aa80299dbdd4a50b
euskal34 <euskal34@hotmail.fr>
2016-01-07 17:01:37 +0100

    Update Couple.java

--- src/projeyy/mst2/Couple.java
+++ src/projeyy/mst2/Couple.java
@@ -5,25 +5,29 @@ public class Couple {
 	private final int i;
 	private final int j;
 	
+	// Constructeur de couple de ville
 	public Couple(int ii, int jj){
 		i=ii;
 		j=jj;
 	}
-
+	
+	// Constructeur vide
 	public Couple() {
 		i=-1;
 		j=-1;
 	}
 	
+	// Constructeur par recopie
 	public Couple (Couple c){
 		i=c.getI();
 		j=c.getJ();
 	}
-
+	// Retourne i
 	public int getI() {
 		return i;
 	}
-
+	
+	// Retourne j
 	public int getJ() {
 		return j;
 	}
