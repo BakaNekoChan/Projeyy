a8db62d2504315f21dbeab6fcb1c8cbb18d418fc
euskal34 <euskal34@hotmail.fr>
2016-01-07 17:20:32 +0100

    Update CoupleDis.java

--- src/projeyy/mst2/CoupleDis.java
+++ src/projeyy/mst2/CoupleDis.java
@@ -3,22 +3,28 @@ package projeyy.mst2;
 
 public class CoupleDis extends Couple implements Comparable<CoupleDis>{
 	private final double val;
-
+	
+	//Constructeur vide de CoupleDis via le constructeur de Couple
 	public CoupleDis(){
 		super(-1,-1);
 		val=-1;
 	}
+	
+	//Constructeur de CoupleDis via le constructeur de Couple
 	public CoupleDis(int ii, int jj, double m){
 		super(ii,jj);
 		val=m;
 	}
-
+	
+	// Retourne la valeur de la distance entre les deux villes du CoupleDis
 	public double getVal(){
 		return val;
 	}
 
 
 	@Override
+	
+	// Retourne un entier permettant de comparer deux CoupleDis 
 	public int compareTo(CoupleDis cd2) {
 		if (val==cd2.val){
 			if (getI()==cd2.getI())
@@ -29,7 +35,7 @@ public class CoupleDis extends Couple implements Comparable<CoupleDis>{
 		else 
 			return (int) ((val-cd2.val)*1000);
 	}
-	
+	// toString pour pouvoir afficher un CoupleDis
 	public String toString(){
 		return "["+getI()+", "+getJ()+"] -> "+val;
 	}
